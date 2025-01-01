5a4bef6789bd459f3aadb90d346f29f1b2d72d7a
Mayun-Binod <shresthabinod334@gmail.com>
2025-01-01 16:21:38 +0545

    message framework learning with CRUD operation

--- crud_project/crud_app/views.py
+++ crud_project/crud_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Student_Detail
+from django.contrib import messages
 
 def home(request):
     stud = Student_Detail.objects.all() 
@@ -13,15 +14,26 @@ def contact(request):
 
 
 def form(request):
-    if request.method == "POST":
-        fm = request.POST
-        fn = fm.get('name') # or fn=fm['name']
-        age = fm.get('age')
-        em = fm.get('email')
-        mssg = fm.get('message')
+    try:
+         if request.method == "POST":
+          fm = request.POST
+          fn = fm.get('name') # or fn=fm['name']
+          age = fm.get('age')
+          em = fm.get('email')
+          mssg = fm.get('message')
 
-        if fn and age and em and mssg:  
+         if int(age)<1 or int(age)>100:
+            messages.error(request,"your age must be between 1-100")
+            return redirect('form')  
+
+         if fn and age and em and mssg:  
             store = Student_Detail(full_name=fn, age=age, email=em, message=mssg)
+            store.full_clean()
             store.save()
-            return redirect('home')  
+            messages.success(request,f"hi {fn} successfully submitted!!!")
+            return redirect('form')
+    except Exception as e:
+        messages.success(request,f"error:{str(e)}")     
+     
     return render(request, 'crud_app/form.html')
+
